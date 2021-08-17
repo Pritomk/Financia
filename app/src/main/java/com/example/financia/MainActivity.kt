e0a40522c031cac17195ef1e8cd9d9bb5e84022e
pritom <pritom.gangarampur@gmail.com>
2021-08-17 22:08:08 +0530

    First push to github

---
+++ app/src/main/java/com/example/financia/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.financia
+
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.financia.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_expense, R.id.navigation_bill_reminder, R.id.navigation_statistics
+            )
+        )
+        val toolbar : Toolbar = findViewById(R.id.my_toolbar)
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/example/financia/ui/billReminder/BillReminderFragment.kt
@@ -0,0 +1,44 @@
+package com.example.financia.ui.billReminder
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.financia.databinding.FragmentBillReminderBinding
+
+class BillReminderFragment : Fragment() {
+
+    private lateinit var billReminderViewModel: BillReminderViewModel
+    private var _binding: FragmentBillReminderBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        billReminderViewModel =
+            ViewModelProvider(this).get(BillReminderViewModel::class.java)
+
+        _binding = FragmentBillReminderBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        billReminderViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
